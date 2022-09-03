c56c6bb90149ca1a861bfee5d1cbe4d8316dfcfb
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-09-03 15:29:26 -0300

    Adiciona Requisito 16

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const input = document.querySelector('#carta-texto');
 const criaCartaBtn = document.querySelector('#criar-carta');
 const cartaGerada = document.querySelector('#carta-gerada');
+const arrayCalss = ['newspaper', 'magazine1', 'magazine2', 'medium', 'big', 'reallybig', 'rotateleft', 'rotateright', 'skewleft', 'skewright'];
 
 function killspan() {
   let child = cartaGerada.firstChild;
@@ -8,6 +9,11 @@ function killspan() {
     cartaGerada.removeChild(child);
     child = cartaGerada.firstChild;
   }
+}
+
+function addClassRandom() {
+  const num = Math.floor(Math.random() * (arrayCalss.length));
+  return arrayCalss[num];
 }
 
 function erro() {
@@ -24,7 +30,8 @@ criaCartaBtn.addEventListener('click', () => {
   const arrayString = string.split(' ');
   for (let i = 0; i < arrayString.length; i += 1) {
     const span = document.createElement('span');
+    span.classList = addClassRandom();
     span.innerText = arrayString[i];
     cartaGerada.appendChild(span);
-  } // input.value = null;
+  }
 });
