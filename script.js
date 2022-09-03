7a46e1be404a4e35a56d7543a48c2b7aab6c5cc4
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-09-03 14:35:24 -0300

    Adiciona requisito 03

---
+++ script.js
@@ -0,0 +1,22 @@
+const input = document.querySelector('#carta-texto');
+const criaCartaBtn = document.querySelector('#criar-carta');
+const cartaGerada = document.querySelector('#carta-gerada');
+
+function killspan() {
+  let child = cartaGerada.firstChild;
+  while (child) {
+    cartaGerada.removeChild(child);
+    child = cartaGerada.firstChild;
+  }
+}
+
+criaCartaBtn.addEventListener('click', () => {
+  killspan();
+  const string = input.value;
+  const arrayString = string.split(' ');
+  for (let i = 0; i < arrayString.length; i += 1) {
+    const span = document.createElement('span');
+    span.innerText = arrayString[i];
+    cartaGerada.appendChild(span);
+  } input.value = null;
+});
